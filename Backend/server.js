ff488777a2cb5b9875891ffa61f2a6c586c52066
orewagaurav <gauravkumar47967@gmail.com>
2025-08-03 17:31:58 +0530

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,32 @@
-import dotenv from 'dotenv';
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
 const app = express();
-dotenv.config();//loads environment variables from .env file into process.env
-const port = process.env.PORT|| 4000; // Fallback to 4000 if PORT is not set in .env
-console.log('PORT from env:', process.env.PORT);
-app.get('/',(req,res)=>{
-    res.send("Hello Gaurav");
-})
-app.listen(port,()=>{
-    console.log(`Active at port : ${port}`);
-    
-})
+dotenv.config(); //loads environment variables from .env file into process.env
+const port = process.env.PORT || 4000;
+console.log("PORT from env:", process.env.PORT);
+
+app.get("/", (req, res) => {
+  res.send("Hello Gaurav");
+});
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      joke: "Why don't scientists trust atoms? Because they make up everything!",
+      title: "Atomic Joke"
+    },
+    {
+      id: 2,
+      joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
+      title: "Scarecrow Award"
+    },
+    { id: 3,
+      joke: "What do you call fake spaghetti? An impasta!" ,
+      title: "Fake Pasta"}
+  ];
+  res.send(jokes);
+
+});
+app.listen(port, () => {
+  console.log(`Active at port : ${port}`);
+});
