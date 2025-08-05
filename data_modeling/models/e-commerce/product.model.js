2a16c48da7578ececa5c036a8754a55d4414f59d
orewagaurav <gauravkumar47967@gmail.com>
2025-08-05 10:37:53 +0530

    e-commerce

---
+++ data_modeling/models/e-commerce/product.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema(
+    {
+        description:{
+            type:String,
+            required:true
+        },
+        name:{
+            type:String,
+            required:true
+        },
+        productImage:{
+            type:String
+        },
+        price:{
+            type:Number,
+            default:0
+        },
+        stock:{
+            type:Number,
+            default:0
+        },
+        category:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Category",
+            required:true
+        },
+        ownership:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    },{timestamps:true})
+
+export const Product = mongoose.mode("Product",productSchema);
