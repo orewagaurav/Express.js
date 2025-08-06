a408e6bd52128ed0f7912419a502420be0e2158d
orewagaurav <gauravkumar47967@gmail.com>
2025-08-07 00:39:46 +0530

    Add order and order item schemas for e-commerce model

--- data_modeling/models/e-commerce/order.models.js
+++ data_modeling/models/e-commerce/order.models.js
@@ -1,2 +1,38 @@
 import mongoose from 'mongoose'
-const orderSchema  = new mongoose
+
+const orderItemSchema = new mongoose.Schema({
+    productId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Product"
+    },
+    quantity:{
+        type:Number,
+        required:true
+    }
+})
+
+const orderSchema  = new mongoose.Schema(
+    {
+        orderPrice:{
+            type: Number,
+            required: true
+        },
+        customer:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        oredrItems:{
+            type:[orderItemSchema]
+        },
+        address:{
+            type:String,
+            required:true
+        },
+        status:{
+            type:String,
+            enum:["PENDING","CANCELLED","DELIVERED"],
+            default:"PENDING"
+        }
+
+    },{timestamps:true});
+export const Order = mongoose.model("Order",orderSchema);
